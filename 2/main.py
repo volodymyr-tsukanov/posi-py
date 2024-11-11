9adea6e1bff8474d1bf1b8616c3a618703efe607
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2024-11-11 20:54:18 +0100

    created tmp.py

--- 2/main.py
+++ 2/main.py
@@ -12,9 +12,25 @@ shuffle = True
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+
+
+def draw_corr(corr_data : pd.DataFrame, title='Corr-Mtx'):
+    cmap = LinearSegmentedColormap.from_list("WiRGn", [(0.85,0.75,0.85,1), (1,0,0,1), (0,1,0,1)])
+    
+    plt.figure(figsize=(corr_data.shape[0], corr_data.shape[1]))
+    plt.title(title, fontsize=22)
+    plt.imshow(corr_data, cmap=cmap, aspect='equal')
+    plt.colorbar()  #right bar
+    plt.xticks(range(len(corr_data.columns)), corr_data.columns, rotation=45)   #axis labels
+    plt.yticks(range(len(corr_data.columns)), corr_data.columns)
+    for (i, j), val in np.ndenumerate(corr_data):   #data labels
+        plt.text(j, i, f'{val:.2f}', ha='center', va='center', color=(0.05,0.15,0,1))
+    plt.grid(False)  #disable gridlines
+    plt.show()
 
 
 data_csv = pd.read_csv("practice_lab_2.csv", sep=";")
@@ -31,6 +47,8 @@ cb = plt.colorbar()
 cb.ax.tick_params(labelsize=10)
 plt.title('Correlation Matrix', fontsize=16);
 plt.show()
+
+draw_corr(cr_mx,'Mieszkania')
 
 
 #zad2.2
